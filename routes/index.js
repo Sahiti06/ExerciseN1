31c74d33b4b83d7b22f8571006d52aad0019d7bd
Stadep <stadepalli@horizon.csueastbay.edu>
2017-10-22 01:50:47 -0700

    TestNodeJSExpress

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,15 @@ var express = require('express');
 var router = express.Router();
 var pg = require('pg');
 var app = require('express')();
-var connectionString = process.env.DATABASE_URL;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-app.get('/db', function (request, response) {
-    var client = new pg.Client(connectionString);
-    client.connect();
-    const query = client.query('SELECT * FROM test_table', function(err, result) {
+router.get('/db', function (request, response) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        client.query('SELECT * FROM test_table', function(err, result) {
             done();
             if (err)
             { console.error(err); response.send("Error " + err); }
@@ -20,5 +18,6 @@ app.get('/db', function (request, response) {
             { response.render('pages/db', {results: result.rows} ); }
         });
     });
+});
 
 module.exports = router;
